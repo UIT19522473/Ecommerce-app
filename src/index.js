17cc2d0d3e82f41baed8d1ef5b7459561809e40d
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-07-11 00:29:08 +0700

    commit slick  slider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 // import { Provider } from "react-redux";
 // import { store } from "./app/store";
 import App from "./App";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
