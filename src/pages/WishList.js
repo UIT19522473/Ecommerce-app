ed5c89cef2d9ce9f4fdfefd7795ce93bb0fa07a6
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-10-23 20:51:30 +0700

    commit

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -23,6 +23,7 @@ const WishList = () => {
 
   useEffect(() => {
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [wishListRedux]);
 
   const handleResize = () => {
